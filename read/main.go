b1f8841084cb5779401b9bdb4bc5ac679d0a0c1d
Vladimir Smirnov <vladimir.smirnov@sociomantic.com>
2014-11-19 14:53:26 +0100

    Some WIP changes.

--- read/main.go
+++ read/main.go
@@ -6,61 +6,71 @@ import (
 	"flag"
 	"log"
 	"math"
-	"math/rand"
-	"net"
 	"os"
 	"runtime"
 	"runtime/pprof"
 	"strconv"
 	"sync"
 	"time"
+	"net/http"
+	"io/ioutil"
 )
 
 var (
-	host                      = flag.String("host", "127.0.0.1:2003", "graphite host")
+	host                      = flag.String("host", "127.0.0.1:80", "graphite-api/web host")
 	cpuprofile                = flag.String("cpuprofile", "", "write cpu profile to file")
-	arg_connections           = flag.Uint64("connections", 10000, "Connections")
-	arg_simultaniously        = flag.Uint64("simul", 1000, "Simultaniously connections")
-	arg_points_per_connection = flag.Uint64("points", 1000, "Datapoints per connection")
+	arg_connections           = flag.Uint64("connections", 10, "Connections")
+	arg_simultaniously        = flag.Uint64("simul", 10, "Simultaniously connections")
+	arg_points_per_connection = flag.Uint64("points", 20, "Datapoints per connection")
 	threads                   = flag.Int("threads", 2, "Threads")
-	runs                      = flag.Uint64("runs", 0, "Number of runs, 0 = infinity")
-	proto                     = flag.String("proto", "tcp", "Protocol, tcp/udp")
+	runs                      = flag.Uint64("runs", 10, "Number of runs, 0 = infinity")
+	proto                     = flag.String("proto", "http", "Protocol, http/https (HTTP strongly adviced)")
     step_con                  = flag.Uint64("stepconnections", 0, "Increase number connections with this step")
     step_points               = flag.Uint64("steppoints", 0, "Increase number of points with this step")
-    do_not_wait               = flag.Bool("nowait", false, "Do not wait after interation")
+	prefix                    = flag.String("prefix", "one_min", "Prefix for metric names")
+	do_not_wait               = flag.Bool("nowait", false, "Do not wait after interation")
 )
 
-func send_data(conn net.Conn, points_per_connection uint64, n uint64) {
+func send_data(points_per_connection uint64, n uint64) {
 	var (
-		i      uint64
-		buf    bytes.Buffer
-		params []string
-		num    int
-		err    error
+		i       uint64
+		buf     bytes.Buffer
+		params  []string
+		req     string
+		err     error
 	)
-	defer conn.Close()
+	defer buf.Reset()
 	date := time.Now().Unix()
-	host_base := "one_min.perf_test.test" + strconv.FormatUint(n, 10) + ".metric"
-	end_str := " " + strconv.FormatFloat(rand.Float64(), 'f', -1, 32) + " " + strconv.FormatInt(date, 10) + "\n"
+	buf.WriteString("/render/?_dummy=" + strconv.FormatInt(date, 10))
+	host_base := "&target=" + *prefix + ".perf_test.test0.metric"
 
 	for i = 0; i < points_per_connection; i++ {
 		params = []string{
 			host_base,
-			strconv.FormatUint(i, 10),
-			end_str,
+			strconv.FormatUint(i+n, 10),
 		}
 		// fmt.Fprintf(conn, host_base+strconv.FormatUint(i, 10)+" "+strconv.FormatFloat(math.Sin(float64(date)+float64(i)), 'f', -1, 32)+" "+date_str+"\n")
 		for _, param := range params {
 			buf.WriteString(param)
 		}
 	}
-
-	num, err = conn.Write(buf.Bytes())
-	if err != nil {
-		log.Println("Written: %i, error: %s", num, err.Error())
-	}
+	req = *proto + "://" + *host + buf.String()
 	buf.Reset()
-
+//	req = *proto + "://" + *host + req
+	resp, err := http.Get(req)
+	if err != nil {
+		log.Println("Request failed:", err)
+		return
+	}
+	defer resp.Body.Close()
+	ioutil.ReadAll(resp.Body)
+	/*
+	res, err := ioutil.ReadAll(resp.Body)
+	log.Println("URL   :", req)
+	log.Println("Code  :", resp.StatusCode)
+	log.Println("Header:", resp.Header)
+	log.Println("Result:", string(res))
+	*/
 	return
 }
 
@@ -68,8 +78,6 @@ func main() {
 	var (
 		i, j, cnt, connections, points_per_connection, simultaniously uint64
 		wg                                                            sync.WaitGroup
-		conn                                                          net.Conn
-		err                                                           error
 		timings                                                       []uint64
 	)
 	flag.Parse()
@@ -96,25 +104,19 @@ func main() {
 	log.Println("Starting...")
         for {
             for {
-                    log.Println("===Iteration", cnt, "/", *runs, "===")
+                    log.Println("===Iteration_", cnt + 1, "/", *runs, "===")
                     begin := time.Now().UnixNano()
-                    log.Println("Load    : ", connections*points_per_connection, "=",  connections, "x", points_per_connection, " metrics")
+					log.Println("Load    : ", connections*points_per_connection, "=",  connections, "x", points_per_connection, " metrics")
 
                     j = 0
                     for j < connections {
                             for i = 0; i < simultaniously; i++ {
                                     j++
-                                    conn, err = net.DialTimeout(*proto, *host, 150*time.Millisecond)
-                                    if err != nil {
-                                            log.Println("GoRoutine ", i, ", error: ", err)
-                                            continue
-                                    }
-
                                     wg.Add(1)
-                                    go func(conn net.Conn, points_per_connection, i uint64) {
+                                    go func(points_per_connection, i uint64) {
                                             defer wg.Done()
-                                            send_data(conn, points_per_connection, i)
-                                    }(conn, points_per_connection, i)
+                                            send_data(points_per_connection, i)
+                                    }(points_per_connection, i)
                                     if j >= connections {
                                             break
                                     }
@@ -126,7 +128,8 @@ func main() {
                     spent := uint64(end - begin)
                     log.Println("Spent   : ", strconv.FormatFloat(float64(spent/1000)/1000/1000, 'f', -1, 32), "seconds")
                     log.Println("Speed   : ", strconv.FormatFloat(float64(connections*points_per_connection)/(float64(spent/1000)/1000/1000), 'f', -1, 32), "metrics/second")
-                    if !*do_not_wait {
+
+					if !*do_not_wait {
                         sleep := uint64(60 * time.Second)
                         if spent < sleep {
                                 sleep -= spent
@@ -136,7 +139,7 @@ func main() {
                                 log.Println("Overtime: ", strconv.FormatFloat(float64(spent-sleep/1000)/1000/1000, 'f', -1, 32), "seconds")
                         }
                     }
-
+	
                     if *runs > 0 {
                             timings[cnt] = spent
                             cnt++
@@ -170,5 +173,4 @@ func main() {
                 cnt = 0
             }
         }
-
 }
