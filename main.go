f0ec5c9b8179cd6703a1961f0b72b2e815368df6
Vladimir Smirnov <vladimir.smirnov@booking.com>
2016-05-24 14:47:59 +0200

    gofmt everything!

--- main.go
+++ main.go
@@ -25,9 +25,9 @@ var (
 	threads                   = flag.Int("threads", 2, "Threads")
 	runs                      = flag.Uint64("runs", 0, "Number of runs, 0 = infinity")
 	proto                     = flag.String("proto", "tcp", "Protocol, tcp/udp")
-    step_con                  = flag.Uint64("stepconnections", 0, "Increase number connections with this step")
-    step_points               = flag.Uint64("steppoints", 0, "Increase number of points with this step")
-    do_not_wait               = flag.Bool("nowait", false, "Do not wait after interation")
+	step_con                  = flag.Uint64("stepconnections", 0, "Increase number connections with this step")
+	step_points               = flag.Uint64("steppoints", 0, "Increase number of points with this step")
+	do_not_wait               = flag.Bool("nowait", false, "Do not wait after interation")
 )
 
 func send_data(conn net.Conn, points_per_connection uint64, n uint64) {
@@ -94,81 +94,80 @@ func main() {
 	cnt = 0
 
 	log.Println("Starting...")
-        for {
-            for {
-                    log.Println("===Iteration", cnt, "/", *runs, "===")
-                    begin := time.Now().UnixNano()
-                    log.Println("Load    : ", connections*points_per_connection, "=",  connections, "x", points_per_connection, " metrics")
+	for {
+		for {
+			log.Println("===Iteration", cnt, "/", *runs, "===")
+			begin := time.Now().UnixNano()
+			log.Println("Load    : ", connections*points_per_connection, "=", connections, "x", points_per_connection, " metrics")
 
-                    j = 0
-                    for j < connections {
-                            for i = 0; i < simultaniously; i++ {
-                                    j++
-                                    conn, err = net.DialTimeout(*proto, *host, 150*time.Millisecond)
-                                    if err != nil {
-                                            log.Println("GoRoutine ", i, ", error: ", err)
-                                            continue
-                                    }
+			j = 0
+			for j < connections {
+				for i = 0; i < simultaniously; i++ {
+					j++
+					conn, err = net.DialTimeout(*proto, *host, 150*time.Millisecond)
+					if err != nil {
+						log.Println("GoRoutine ", i, ", error: ", err)
+						continue
+					}
 
-                                    wg.Add(1)
-                                    go func(conn net.Conn, points_per_connection, i uint64) {
-                                            defer wg.Done()
-                                            send_data(conn, points_per_connection, i)
-                                    }(conn, points_per_connection, i)
-                                    if j >= connections {
-                                            break
-                                    }
-                            }
-                            wg.Wait()
-                    }
+					wg.Add(1)
+					go func(conn net.Conn, points_per_connection, i uint64) {
+						defer wg.Done()
+						send_data(conn, points_per_connection, i)
+					}(conn, points_per_connection, i)
+					if j >= connections {
+						break
+					}
+				}
+				wg.Wait()
+			}
 
-                    end := time.Now().UnixNano()
-                    spent := uint64(end - begin)
-                    log.Println("Spent   : ", strconv.FormatFloat(float64(spent/1000)/1000/1000, 'f', -1, 32), "seconds")
-                    log.Println("Speed   : ", strconv.FormatFloat(float64(connections*points_per_connection)/(float64(spent/1000)/1000/1000), 'f', -1, 32), "metrics/second")
-                    if !*do_not_wait {
-                        sleep := uint64(60 * time.Second)
-                        if spent < sleep {
-                                sleep -= spent
-                                log.Println("Sleeping: ", strconv.FormatFloat(float64(sleep/1000)/1000/1000, 'f', -1, 32), "seconds")
-                                time.Sleep(time.Duration(sleep))
-                        } else {
-                                log.Println("Overtime: ", strconv.FormatFloat(float64(spent-sleep/1000)/1000/1000, 'f', -1, 32), "seconds")
-                        }
-                    }
+			end := time.Now().UnixNano()
+			spent := uint64(end - begin)
+			log.Println("Spent   : ", strconv.FormatFloat(float64(spent/1000)/1000/1000, 'f', -1, 32), "seconds")
+			log.Println("Speed   : ", strconv.FormatFloat(float64(connections*points_per_connection)/(float64(spent/1000)/1000/1000), 'f', -1, 32), "metrics/second")
+			if !*do_not_wait {
+				sleep := uint64(60 * time.Second)
+				if spent < sleep {
+					sleep -= spent
+					log.Println("Sleeping: ", strconv.FormatFloat(float64(sleep/1000)/1000/1000, 'f', -1, 32), "seconds")
+					time.Sleep(time.Duration(sleep))
+				} else {
+					log.Println("Overtime: ", strconv.FormatFloat(float64(spent-sleep/1000)/1000/1000, 'f', -1, 32), "seconds")
+				}
+			}
 
-                    if *runs > 0 {
-                            timings[cnt] = spent
-                            cnt++
-                            if cnt >= *runs {
-                                    mean := float64(0.0)
-                                    std := float64(0.0)
-                                    cnt = 0
-                                    for _, t := range timings {
-                                            cnt++
-                                            mean += float64(t / 1000)
-                                    }
-                                    mean = mean / float64(cnt)
-                                    for _, t := range timings {
-                                            std += math.Pow(float64(t/1000)-mean, 2.0)
-                                    }
-                                    std = math.Sqrt(1 / float64(cnt) * std)
-                                    log.Println("Result  : ", strconv.FormatFloat(mean/1000/1000, 'f', -1, 32), "+-", strconv.FormatFloat(std/1000/1000, 'f', -1, 32), "seconds")
-                                    break
-                            }
-                    }
-            }
+			if *runs > 0 {
+				timings[cnt] = spent
+				cnt++
+				if cnt >= *runs {
+					mean := float64(0.0)
+					std := float64(0.0)
+					cnt = 0
+					for _, t := range timings {
+						cnt++
+						mean += float64(t / 1000)
+					}
+					mean = mean / float64(cnt)
+					for _, t := range timings {
+						std += math.Pow(float64(t/1000)-mean, 2.0)
+					}
+					std = math.Sqrt(1 / float64(cnt) * std)
+					log.Println("Result  : ", strconv.FormatFloat(mean/1000/1000, 'f', -1, 32), "+-", strconv.FormatFloat(std/1000/1000, 'f', -1, 32), "seconds")
+					break
+				}
+			}
+		}
 
-
-            if *step_con == 0 && *step_points == 0 {
-                break
-            } else {
-                connections += *step_con
-                points_per_connection += *step_points
-                log.Println("Increasing load by ", *step_con, " connections and ", *step_points, " points per connection")
-                log.Println("Load    : ", connections, "x", points_per_connection, "=", connections*points_per_connection, " metrics")
-                cnt = 0
-            }
-        }
+		if *step_con == 0 && *step_points == 0 {
+			break
+		} else {
+			connections += *step_con
+			points_per_connection += *step_points
+			log.Println("Increasing load by ", *step_con, " connections and ", *step_points, " points per connection")
+			log.Println("Load    : ", connections, "x", points_per_connection, "=", connections*points_per_connection, " metrics")
+			cnt = 0
+		}
+	}
 
 }
